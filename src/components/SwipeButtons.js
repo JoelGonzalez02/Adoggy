a75da94c71850bf8a9183b935b136fba11d76210
unknown <espjoemusic@gmail.com>
2021-07-19 23:18:29 -0700

    adding react touch events

--- src/components/SwipeButtons.js
+++ src/components/SwipeButtons.js
@@ -3,6 +3,7 @@ import './SwipeButtons.css';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import PageviewIcon from '@material-ui/icons/Pageview';
 // import { Button } from '@material-ui/core';
+import ReactTouchEvents from "react-touch-events";
 
 
 const SwipeButtons = (props) => {
@@ -12,14 +13,16 @@ const SwipeButtons = (props) => {
             {/* <Button  className='page' startIcon={<PageviewIcon/>} style={{color: 'rgb(38, 64, 179)'}} >
                 <a href={`${props.url}`} target='_blank' rel="noreferrer" style={{textDecoration: 'none'}}>View Profile</a>
             </Button> */}
-
-            <button  className='page' style={{color: 'rgb(38, 64, 179)', cursor: 'pointer'}}>
+            <ReactTouchEvents onTap={() => window.open(`${props.url}`)} >
+                <button type='button' className='page' style={{color: 'rgb(38, 64, 179)', cursor: 'pointer', zIndex: '999'}}>
                 <div className='inButtons'>
                      
                      <a href={`${props.url}`} target='_blank' rel="noreferrer" style={{textDecoration: 'none'}}><PageviewIcon className='pIcon' />Profile</a>
                 </div>
                
             </button>
+            </ReactTouchEvents>
+            
 
 
             <button className='fav' style={{color: '#ef32d9'}}>
