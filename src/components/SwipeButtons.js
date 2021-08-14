030f3886074e499cb01e3bbfbf39562942084d01
unknown <espjoemusic@gmail.com>
2021-08-14 15:39:32 -0700

    adding new pages

--- src/components/SwipeButtons.js
+++ src/components/SwipeButtons.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './SwipeButtons.css';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import PageviewIcon from '@material-ui/icons/Pageview';
-// import { Button } from '@material-ui/core';
 import ReactTouchEvents from "react-touch-events";
 
 
@@ -10,9 +9,7 @@ const SwipeButtons = (props) => {
     
     return (
         <div className='swipe-buttons'>
-            {/* <Button  className='page' startIcon={<PageviewIcon/>} style={{color: 'rgb(38, 64, 179)'}} >
-                <a href={`${props.url}`} target='_blank' rel="noreferrer" style={{textDecoration: 'none'}}>View Profile</a>
-            </Button> */}
+        
             <ReactTouchEvents onTap={() => window.open(`${props.url}`)} >
                 <button type='button' className='page' style={{color: 'rgb(38, 64, 179)', cursor: 'pointer', zIndex: '999'}}>
                 <div className='inButtons'>
@@ -33,12 +30,6 @@ const SwipeButtons = (props) => {
                 
             </button>
 
-
-   
-                
-            {/* <Button className='fav' startIcon={<FavoriteIcon/>} style={{color: '#ef32d9'}}>
-                    Like
-            </Button> */}
           
         </div>
     )
